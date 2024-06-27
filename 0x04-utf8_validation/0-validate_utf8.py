ce71b0bbe71445d27551e655c8a5de28a221421a
DevBekoDev <48620096+DevBekoDev@users.noreply.github.com>
2024-06-28 01:49:51 +0300

    final result

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,29 +1,58 @@
 #!/usr/bin/python3
 """
-check a seqance of an inpit is utf-8 valid
+Validate UTF8
 """
 
 
 def validUTF8(data):
     """
-    using the bit manipualtion to validate utf8 encoding
+    Validate if data contains valid UTF-8 characters
     """
+    isFirstByte = True  # check if we processing the first byte of a character
+    remainingBytes = 0  # Number of remaining bytes for the current character
+    binaryData = []
 
-    count = 0
-    for n in data:
-        if count == 0:
-            if (n >> 7) == 0:
-                count = 0
-            elif (n >> 5) == 0x06:
-                count = 1
-            elif (n >> 4) == 0x0e:
-                count = 2
-            elif (n >> 3) == 0x1e:
-                count = 3
-            else:
-                return False
+    # Convert each integer in data to its 8-bit binary representation
+    for number in data:
+        binaryData.append('{0:08b}'.format(number & 255))
+
+    # Process each binary string in binaryData
+    for binary in binaryData:
+        # Count the leading 1's in the binary string
+        currentByteCount = countLeadingOnes(binary)
+
+        if isFirstByte:
+            # If it's the first byte, determine the expected length of the
+            # character
+            if currentByteCount == 0:
+                continue  # Single-byte character, continue to next byte
+
+            if 2 <= currentByteCount <= 4:
+                remainingBytes = currentByteCount - 1
+                isFirstByte = False  # Next bytes should be continuation bytes
+                continue
+
+            return False  # Invalid first byte
         else:
-            if (n >> 6) != 0x02:
-                return False
-            count -= 1
-    return count == 0
+            # If it's not the first byte, it should be a continuation byte
+            if binary[:2] == '10':
+                remainingBytes -= 1
+            else:
+                return False  # Invalid continuation byte
+
+            if remainingBytes == 0:
+                isFirstByte = True  # bytes of current character are processed
+
+    return isFirstByte
+
+
+def countLeadingOnes(binary):
+    """
+    Count the number of leading 1's in the binary string
+    """
+    count = 0
+    for bit in binary:
+        if bit == '0':
+            break
+        count += 1
+    return count
