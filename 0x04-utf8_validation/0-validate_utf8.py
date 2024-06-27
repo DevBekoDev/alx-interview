0034fe0e9e6458588c76eb2a6b01e403adf9a5c1
DevBekoDev <48620096+DevBekoDev@users.noreply.github.com>
2024-06-28 01:03:15 +0300

    fixed pep8 format

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -3,21 +3,22 @@
 validate if a sequance of input follows the utf-8 encoding pattern
 """
 
+
 def validUTF8(data):
     n = len(data)
     next = 0
     for i in data:
         if next == 0:
-            if (i>>5)==0b110:#for 2 bytes
+            if (i >> 5) == 0b110:  # for 2 bytes
                 next = 1
-            elif (i>>4)==0b1110:#for 3 bytes
+            elif (i >> 4) == 0b1110:  # for 3 bytes
                 next = 2
-            elif (i>>3)==0b11110:#for 4 bytes
+            elif (i >> 3) == 0b11110:  # for 4 bytes
                 next = 3
-            elif (i>>7)!= 0b0:
+            elif (i >> 7) != 0b0:
                 return False
         else:
-            if (i>>6)!= 0b10:#checking next occurence
+            if (i >> 6) != 0b10:  # checking next occurence
                 return False
             next -= 1
     if next == 0:
