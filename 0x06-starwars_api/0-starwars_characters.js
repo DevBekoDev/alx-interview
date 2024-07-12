bf1abb64b74f2546b786db0c0b64be50ba009a06
DevBekoDev <48620096+DevBekoDev@users.noreply.github.com>
2024-07-12 03:13:53 +0300

    made sure the order doesnt change

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -3,47 +3,46 @@
 const { error } = require('console');
 const { request } = require('http');
 const process = require('process');
-const { json } = require('stream/consumers');
-if(process.argv.length <= 2 || process.argv.length > 3)
-    {
-        console.log("Enter movie id => Movie ID - example: 3 = “Return of the Jedi”");
-    }
-else if(isNaN(process.argv[2]))
-    {
-        console.log("Enter movie id => Movie ID - example: 3 = “Return of the Jedi”");
-    }
-else if(process.argv[2] <0 || process.argv[2] > 7)
-{
+
+if (process.argv.length <= 2 || process.argv.length > 3) {
+    console.log("Enter movie id => Movie ID - example: 3 = “Return of the Jedi”");
+} else if (isNaN(process.argv[2])) {
+    console.log("Enter movie id => Movie ID - example: 3 = “Return of the Jedi”");
+} else if (process.argv[2] < 1 || process.argv[2] > 7) {
     console.log("Enter movie id between 1 and 7 => Movie ID - example: 3 = “Return of the Jedi”");
-}
-else
-{
+} else {
     const request = require('request');
     
     let url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2] + '/';
 
-    request(url, (error,response, body) => 
-        {
-            if(error)
-                {
-                    console.log(error);
-                }
-            //console.log(response.statuscode);
-            //console.log(body);
-            let result = JSON.parse(body);
-            
-            for(let i = 0; i < result.characters.length; i ++)
-                {
-                    request(result.characters[i], (error,response,content) =>
-                        {
-                            if(error)
-                            {
-                                console.log(error);
-                            }
-                            let names = JSON.parse(content);
-                            console.log(names.name);
-                            
-                        });
-                }
+    request(url, (error, response, body) => {
+        if (error) {
+            console.log(error);
+            return;
+        }
+        
+        let result = JSON.parse(body);
+        let characterPromises = result.characters.map((characterUrl) => {
+            return new Promise((resolve, reject) => {
+                request(characterUrl, (error, response, content) => {
+                    if (error) {
+                        reject(error);
+                        return;
+                    }
+                    let names = JSON.parse(content);
+                    resolve(names.name);
+                });
+            });
         });
+
+        Promise.all(characterPromises)
+            .then((characterNames) => {
+                characterNames.forEach((name) => {
+                    console.log(name);
+                });
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    });
 }
