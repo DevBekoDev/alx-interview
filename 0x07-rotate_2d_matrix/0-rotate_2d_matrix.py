6bb3be22f338161c1fd0406388fb8f3e5a85b3a1
DevBekoDev <48620096+DevBekoDev@users.noreply.github.com>
2024-07-19 01:57:35 +0300

    pycodestyle

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -2,11 +2,14 @@
 """script tp rotate a 2D Matrix
 """
 
+
 def rotate_2d_matrix(matrix):
     """taking nXn matrix,rotate the matric 90 degrees clockwise."""
     rotated = [list(row) for row in zip(*reversed(matrix))]
     for i in range(len(matrix)):
         matrix[i] = rotated[i]
+
+
 if __name__ == '__main__':
     matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 
