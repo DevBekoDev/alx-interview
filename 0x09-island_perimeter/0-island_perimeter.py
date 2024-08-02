b96d80aa220488b9acc9874a035d880a80ea44aa
DevBekoDev <48620096+DevBekoDev@users.noreply.github.com>
2024-08-02 04:22:03 +0300

    island paramter

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+island parameter
+"""
+
+
+def island_perimeter(grid):
+    R, C = len(grid), len(grid[0])
+    perimeter = 0
+    # Traverse the grid
+    for i in range(R):
+        for j in range(C):
+            if grid[i][j] == 1:
+                perimeter += 4
+                # Check whether top neighbour is a land and decrement it by 2
+                # as it intersects
+                if i > 0 and grid[i-1][j] == 1:
+                    perimeter -= 2
+                # Check left neighbour is a land and decrement it by 2
+                # as it intersects
+                if j > 0 and grid[i][j-1] == 1:
+                    perimeter -= 2
+    return perimeter
